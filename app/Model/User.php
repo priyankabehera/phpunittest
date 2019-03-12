bb1635810c72d059158ef4d219a22359bef9b459
priyankabehera <priyanka.behera@multidots.com>
2019-03-12 16:40:46 +0530

    Full name test case

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,6 +6,8 @@ class User {
 
 	public $first_name;
 
+	public $last_name;
+
 	public function setFirstName( $FirstName ) {
 
 		$this->first_name = $FirstName;
@@ -14,5 +16,20 @@ class User {
 	public function getFirstName() {
 
 		return $this->first_name;
+	}
+
+	public function setLastName( $LastName ) {
+
+		$this->last_name = $LastName;
+	}
+
+	public function getLastName()   {
+
+		return $this->last_name;
+	}
+
+	public function getFullName(){
+
+		return $this->first_name . ' ' . $this->last_name;
 	}
 }
--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -11,4 +11,24 @@ class UserTest extends \PHPUnit\Framework\TestCase {
 		$this->assertEquals( $user->getFirstName(), 'Billy' );
 
 	}
+
+	public function testThatWeCanGetTheLastName() {
+
+		$user = new \App\Model\User;
+
+		$user->setLastName( "Behera" );
+
+		$this->assertEquals( $user->getLastName(), 'Behera' );
+	}
+
+	public function testThatWeCanGetTheFullName() {
+
+		$user = new \App\Model\User;
+
+		$user->setFirstName( 'Billy' );
+
+		$user->setLastName( 'Behera' );
+
+		$this->assertEquals( $user->getFullName(), 'Billy Behera' );
+	}
 }
